16ecab0bd087eb6f0d9ef750f47694700ace9d1c
Joe Knox <jknox13@uw.edu>
2020-01-09 08:36:25 -0800

    reverse insertion sort

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,7 +1,7 @@
 """Module containing sorting algorithms implemented in Python"""
 
 
-def insertion_sort(arr):
+def insertion_sort(arr, reverse=False):
     """Insertion Sort.
 
     Features:
@@ -18,11 +18,30 @@ def insertion_sort(arr):
             Worst: O(N^2)
         Space: O(1)
     """
+    def stop_swap(j):
+        """given an index, is the value at the index sorted?"""
+        if reverse:
+            return arr[j - 1] > arr[j]
+        return arr[j - 1] < arr[j]
+
     # start at the second element in the array
     for i in range(1, len(arr)):
         for j in range(i, 0, -1):
-            if arr[j-1] < arr[j]:
+            if stop_swap(j):
                 break
 
+            # swap left
             arr[j - 1], arr[j] = arr[j], arr[j - 1]
     return arr
+
+
+def merge_sort(arr):
+    pass
+
+
+def quick_sort(arr):
+    pass
+
+
+def heap_sort(arr):
+    pass
