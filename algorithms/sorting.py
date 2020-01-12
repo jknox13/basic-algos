27b0c084f2559fee705d5190eca6544472971f22
Joe Knox <jknox13@uw.edu>
2020-01-12 13:58:17 -0800

    quick sort

--- algorithms/sorting.py
+++ algorithms/sorting.py
@@ -1,4 +1,5 @@
 """Module containing sorting algorithms implemented in Python"""
+_MEDIAN_OF_THREE_THRESHOLD = 30  # should be emperically chosen
 
 
 def insertion_sort(arr, reverse=False):
@@ -36,6 +37,18 @@ def insertion_sort(arr, reverse=False):
 
 
 def merge_sort(arr):
+    """Merge Sort (recursive implementation)
+
+    Features:
+        - stable
+
+    Complexities:
+        Time:
+            Best:
+            Average:
+            Worst:
+        Space: O(N)
+    """
     def merge(L, R):
         n, m = len(L), len(R)
         while True:
@@ -63,9 +76,85 @@ def merge_sort(arr):
         merge(L, R)
 
 
+def _partition(arr, lo, hi):
+    """unlike lomuto, results in best case if array is sorted and handles
+        repeated elements well
+
+    overview: work inward from ends, inverting if elements should be swapped
+    """
+    # median of 3 partitioning
+    def median_of_three():
+        # place median at end and return median(low, high, middle)
+        mid = (hi + lo) // 2
+        if arr[hi] < arr[lo]:
+            arr[lo], arr[hi] = arr[hi], arr[lo]
+        if arr[mid] < arr[lo]:
+            arr[lo], arr[mid] = arr[mid], arr[lo]
+        if arr[hi] < arr[mid]:
+            arr[hi], arr[mid] = arr[mid], arr[hi]
+        return arr[mid]
+
+    if hi - lo > _MEDIAN_OF_THREE_THRESHOLD:
+        pivot = median_of_three()
+    else:
+        # choose middle
+        pivot = arr[(hi + lo) // 2]
+
+    i, j = lo - 1, hi + 1
+    while True:
+        while True:
+            # move inward from start
+            i += 1
+            if arr[i] >= pivot:
+                break
+        while True:
+            # move inward from end
+            j -= 1
+            if arr[j] <= pivot:
+                break
+        if i >= j:
+            return j
+
+        # swap
+        arr[i], arr[j] = arr[j], arr[i]
+
+
+def _quick_sort_recursive(arr, lo, hi):
+    """recursive quick sort helper.
+    lo: starting index
+    hi: ending index
+    """
+    if lo < hi:
+        pi = _partition(arr, lo, hi)
+
+        _quick_sort_recursive(arr, lo, pi)
+        _quick_sort_recursive(arr, pi + 1, hi)
+
+
 def quick_sort(arr):
-    pass
+    """Merge Sort
+
+    Features:
+
+    Complexities:
+        Time:
+            Best:
+            Average:
+            Worst:
+        Space: O(N)
+    """
+    _quick_sort_recursive(arr, 0, len(arr) - 1)
+    return arr
 
 
 def heap_sort(arr):
     pass
+
+
+def tim_sort(arr):
+    """Hybrid insertion sort and merge sort.
+
+    Features:
+    - take advantage of quadratic time for small arrays
+    - stable (that's why we use merge sort)
+    """
